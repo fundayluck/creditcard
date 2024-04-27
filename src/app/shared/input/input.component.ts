b6bfcc7d962dd5884ff7af3db1194102fe323b70
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-27 18:57:33 +0700

    using library for masking

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective],
+  providers: [provideNgxMask()],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css',
 })
